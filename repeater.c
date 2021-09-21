69a01b484633e72c66f0a1c29a5bbcd20e8e5c5d
kenji801448 <74351307+kenji801448@users.noreply.github.com>
2021-09-22 07:18:58 +0800

    Update repeater.c

--- repeater.c
+++ repeater.c
@@ -4,6 +4,9 @@ int main() {
     
     int input;
 
+    //fill some condition into the following while loop
+    //only ONE line code is you need
+    //please refer page 4 of homework request
     while () {
         printf("%d\n", input);
     }
