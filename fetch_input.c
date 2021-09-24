af79369dc5195c34cd55c4ed7921d6b5b973aa41
kenji801448 <74351307+kenji801448@users.noreply.github.com>
2021-09-24 11:37:47 +0800

    Update fetch_input.c

--- fetch_input.c
+++ fetch_input.c
@@ -17,3 +17,23 @@ int main() {
     printf("d=%d\n", d);
     printf("e=%d\n", e);
 }
+
+#include <stdio.h>
+
+int main() {
+    
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int d = 0;
+    int e = 0;
+
+    //add scanf code to get data from input
+    //and feed those input into a, b, c, d, e, respectively.
+
+    printf("a=%d\n", a);
+    printf("b=%d\n", b);
+    printf("c=%d\n", c);
+    printf("d=%d\n", d);
+    printf("e=%d\n", e);
+}
