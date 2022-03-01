40eeaa2953ee26b4c01d7b9e5fcdbf1241cdf70d
kenji801448 <kenji801448@tmail.ilc.edu.tw>
2022-03-01 15:45:41 +0800

    Update repeater.c

--- repeater.c
+++ repeater.c
@@ -7,7 +7,6 @@ int main() {
     printf("每輸入一個數字，程式會再印出相同的數字。\n");
     //fill some condition into the following while loop
     //only ONE line code is you need
-    //please refer page 4 of homework request
     while () {
         printf("%d\n", input);
     }
