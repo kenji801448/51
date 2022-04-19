b7a6847f7cc76b2824557a73a70690b258741ba7
kenji801448 <kenji801448@tmail.ilc.edu.tw>
2022-04-19 14:41:59 +0800

    Update unit_place.c

--- unit_place.c
+++ unit_place.c
@@ -8,6 +8,6 @@ int main() {
 
     scanf("%d", &i);
     
-    //line 12 is incomplete, refer to page 4 of Operation slides to achieve the requirement.
+    //line 12 is incomplete, refer to page 6 of Operation slides to achieve the requirement.
     printf("這個整數的個位數是%d\n", i);
 }
